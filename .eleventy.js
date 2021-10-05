285492ae18fec46c8896eedee8371bb3e758a714
Paul <paul@theapplegates.com>
2021-10-05 17:24:10 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,61 @@ const Image = require("@11ty/eleventy-img");
 const path = require('path');
 const embedTwitter = require("eleventy-plugin-embed-twitter");
 
+async function imageShortcode(src, alt) {
+  let sizes = "(min-width: 1024px) 100vw, 50vw"
+  let srcPrefix = `./src/img/`
+  src = srcPrefix + src
+  console.log(`Generating image(s) from:  ${src}`)
+  if(alt === undefined) {
+    // Throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+  }
+  let metadata = await Image(src, {
+    widths: [300, 600, 900, 1500, 3000],
+    formats: ['avif', 'webp', 'jpg'],
+    urlPath: "/img/",
+    outputDir: "./_site/img/",
+    /* =====
+    Now we'll make sure each resulting file's name will
+    make sense to you. **This** is why you need
+    that `path` statement mentioned earlier.
+    ===== */
+    filenameFormat: function (id, src, width, format, options) {
+      const extension = path.extname(src)
+      const name = path.basename(src, extension)
+      return `${name}-${width}w.${format}`
+    }
+  })
+  let lowsrc = metadata.jpeg[0]
+  let highsrc = metadata.jpeg[metadata.jpeg.length - 1]
+  return `<picture>
+    ${Object.values(metadata).map(imageFormat => {
+      return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+    }).join("\n")}
+    <img
+      src="${lowsrc.url}"
+      width="${highsrc.width}"
+      height="${highsrc.height}"
+      alt="${alt}"
+      loading="lazy"
+      decoding="async">
+  </picture>`
+}
+
+
+
+
 module.exports = function(eleventyConfig) {
+  
+  eleventyConfig.addPlugin(embedTwitter);
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode)
+  eleventyConfig.addLiquidShortcode("image", imageShortcode)
+  // === Liquid needed if `markdownTemplateEngine` **isn't** changed from Eleventy default
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode)
+
+
+  
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
